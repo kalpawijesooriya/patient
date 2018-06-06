03118ef9ccf2b4380df761bfb26dcc31a1ef6ab8
kalpawijesooriya <kalpa.wijesooriya@yahoo.com>
2018-06-06 15:14:30 +0530

    make paid Affter payments
    make paid Affter payments

--- app/src/main/java/com/project/kalpawijesooriya/patient/AppoinmentDetails.java
+++ app/src/main/java/com/project/kalpawijesooriya/patient/AppoinmentDetails.java
@@ -1,11 +1,22 @@
 package com.project.kalpawijesooriya.patient;
 
+import android.app.ProgressDialog;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,9 +24,21 @@ import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.HashMap;
 import java.util.Map;
 
 
@@ -23,20 +46,25 @@ public class AppoinmentDetails extends AppCompatActivity {
     private Toolbar mToolbar;
     private String consultationID;
     private FirebaseAuth mAuth;
-   private DatabaseReference ref;
+   private StorageReference ref;
    private Button  confirm;
+   String number,timeperpatient;
    String Totalfee;
-private String consulID;
+    FirebaseStorage storage;
+    StorageReference storageReference;
+    private String consulID;
     private Firebase mdatabase;
     private Firebase doctorref;
+    ImageView imageView4,paid;
     private Firebase sheduleref;
    private TextView docName,special,datetext,startTime,apxTime,doc_fee,hos_fee,appointmentNo,roomNo,totalfee;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appoinment_details);
-
-
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+        imageView4=(ImageView)findViewById(R.id.imageView4);
         mToolbar = (Toolbar) findViewById(R.id.consultation_page_toolbar);
         setSupportActionBar(mToolbar);
         mToolbar = (Toolbar) findViewById(R.id.consultation_page_toolbar);
@@ -56,8 +84,9 @@ private String consulID;
         roomNo = (TextView) findViewById(R.id.roomNO);
         confirm=(Button)findViewById(R.id.confirm);
         totalfee=(TextView)findViewById(R.id.totalFee);
+        paid=(ImageView)findViewById(R.id.paid);
         Bundle b = getIntent().getExtras();
-
+        paid.setVisibility(View.GONE);
 
         if (b != null) {
 
@@ -72,6 +101,8 @@ private String consulID;
             totalfee.setText(b.getString("fee")+".00");
             consulID =b.getString("consulID");
             Totalfee=b.getString("fee");
+            timeperpatient= b.getString("timeperpatient");
+            number= b.getString("number");
 
 
         }
@@ -80,16 +111,24 @@ private String consulID;
      confirm.setOnClickListener(new View.OnClickListener() {
          @Override
          public void onClick(View v) {
+             paid.setVisibility(View.VISIBLE);
+             confirm.setVisibility(View.GONE);
+             View rootView = getWindow().getDecorView().findViewById(android.R.id.content);
+             Bitmap bitmap=  takeScreenshot();
+             imageView4.setImageBitmap(bitmap);
              Intent intent = new Intent(AppoinmentDetails.this, Payments.class);
              Bundle b = new Bundle();
              b.putString("total",Totalfee);
 
-             b.putString("timeperpatient",b.getString("timeperpatient"));
-             b.putString("number",b.getString("number") );
+             b.putString("timeperpatient",timeperpatient);
+             b.putString("number",number );
              b.putString("consulID",consulID);
              intent.putExtras(b);
              startActivity(intent);
              finish();
+
+           //  store(bitmap, "DCIM");
+
 
          }
      });
@@ -106,4 +145,56 @@ private String consulID;
         super.onStart();
 
     }
+    public static Bitmap getScreenShot(View view) {
+        View screenView = view.getRootView();
+        screenView.setDrawingCacheEnabled(true);
+        Bitmap bitmap = Bitmap.createBitmap(screenView.getDrawingCache());
+        screenView.setDrawingCacheEnabled(false);
+        return bitmap;
+    }
+
+    public static Bitmap loadBitmapFromView(View v) {
+        Bitmap b = Bitmap.createBitmap(700, 1000, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(b);
+        v.layout(-100, -90, v.getLayoutParams().width, v.getLayoutParams().height);
+        v.draw(c);
+        return b;
+
+    }
+
+    private Bitmap takeScreenshot() {
+
+        Bitmap bitmap = null;
+
+        try {
+
+            String mPath = Environment.getExternalStorageDirectory().toString() + "/"  + ".jpg";
+
+            View u = findViewById(R.id.rel);
+            u.setDrawingCacheEnabled(true);
+            RelativeLayout z = (RelativeLayout) findViewById(R.id.rel);
+            //z.setBackgroundColor(getResources().getColor(R.color.white));
+            int totalHeight = z.getChildAt(0).getHeight();
+            int totalWidth = z.getChildAt(0).getWidth();
+            u.layout(0, 0, totalWidth, totalHeight);
+            u.buildDrawingCache(true);
+            bitmap = Bitmap.createBitmap(u.getDrawingCache());
+            u.setDrawingCacheEnabled(false);
+
+            File imageFile = new File(mPath);
+
+            FileOutputStream outputStream = new FileOutputStream(imageFile);
+            int quality = 100;
+            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream);
+            outputStream.flush();
+            outputStream.close();
+           
+        } catch (Throwable e) {
+
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
+
+
 }
--- app/src/main/java/com/project/kalpawijesooriya/patient/Payments.java
+++ app/src/main/java/com/project/kalpawijesooriya/patient/Payments.java
@@ -1,14 +1,34 @@
 package com.project.kalpawijesooriya.patient;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Payments extends AppCompatActivity {
     TextView amout;
     String con_ID;
     String time;
     String number;
+    Button submit;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,14 +37,40 @@ public class Payments extends AppCompatActivity {
 
         Bundle b = getIntent().getExtras();
         amout = (TextView) findViewById(R.id.amount);
-
+        submit=(Button)findViewById(R.id.submit);
 
         if (b != null) {
 
             amout.setText(b.getString("total")+".00");
             con_ID=b.getString("consulID");
-            time=b.getString("number");
-            number= b.getString("timeperpatient");
+            number =b.getString("number");
+            time = b.getString("timeperpatient");
         }
+        FirebaseUser currentUser= FirebaseAuth.getInstance().getCurrentUser();
+        final String uid=currentUser.getUid();
+        mDatabase= FirebaseDatabase.getInstance().getReference().child("consultation");
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+                                                             @Override
+                                                             public void onDataChange(DataSnapshot dataSnapshot) {
+                                                                 Map<String, Object>  usermap = new HashMap<String,Object>();
+                                                                 usermap.put("patient_ID",uid);
+                                                                 usermap.put("Time",time);
+                                                                 usermap.put("Number",number);
+                                                                 mDatabase.child(con_ID).child("Appointment").child(uid).updateChildren( usermap);
+
+                                                             }
+
+                                                             @Override
+                                                             public void onCancelled(DatabaseError databaseError) {}
+                                                         }
+                );
+            }
+        });
+
+
     }
 }
